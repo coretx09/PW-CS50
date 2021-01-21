c50257db8fe06135506b053f1e5c9d397a51c3fd
sauve <ngampiosauvet@gmail.com>
2021-01-21 21:17:56 +0300

    python

---
+++ PYTHON/hello.py
@@ -0,0 +1,25 @@
+print("hello world")
+
+# ENNTRER eT VARIABLES 
+name = input("your name: ")
+print(f"Hello, {name}")
+
+
+# CONDITIONS 
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else :
+    print("n is zeroh")
+
+
+# SEQUENCES
+name = "Harry"
+
+print(name[0])
+
+names = ["Harry", "Mark", "Josh", "Mike"]
+cordinates = (10.2, 20.7)
