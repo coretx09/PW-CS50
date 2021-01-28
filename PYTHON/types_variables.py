cbf8504c7244db01dc8b7c76dfad2693018559ad
sauve <ngampiosauvet@gmail.com>
2021-01-28 23:36:51 +0300

    j

--- PYTHON/types_variables.py
+++ PYTHON/types_variables.py
@@ -1,3 +1,19 @@
+#Variable
+"""Ce terme désigne le fait d’attribuer un nom ou identificateur à des informations:
+ en les nommant, on peut manipuler ces informations beaucoup plus facilement.
+
+ Une variable est caractérisée par :
+ -Un IDENTIFICATEUR : il peut contenir des lettres, des chiffres, des blancs soulignés
+   mais il ne peut commencer par un chiffre.
+ -Un TYPE : c’est une information sur le contenu de la variable qui indique à l’interpréteur
+   python, la manière de manipuler cette information.
+"""
+x = 14
+y = 45,0
+z = True
+name = 'sauvet007' 
+
+
 #List
 """Les éléments de liste sont ordonnés, modifiables et autorisent
  les valeurs en double.
@@ -33,4 +49,4 @@ print(len(ss))
   sont présentés dans des paires clé: valeur et peuvent être
   référencés à l'aide du nom de clé
 """
-houses = {"Harry":  "Nice", "Ron": "horrible"}
+houses = {"Harry":  "nice", "Ron": "horrible"}
