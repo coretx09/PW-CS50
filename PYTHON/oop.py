c15a91d341eb8a1b2ed50b6c7f7464a470fa1ae2
sauve <ngampiosauvet@gmail.com>
2021-02-02 23:39:07 +0300

    oop

---
+++ PYTHON/oop.py
@@ -0,0 +1,24 @@
+#OOP
+'''La programmation orientée objet (OOP) permet de créer des objets que l'on peut manipuler .
+ La OOP est un paradigme de programmation qui fournit un moyen de structurer
+ les programmes afin que les propriétés et les comportements soient regroupés dans des objets individuels .
+
+ La OOP se concentre sur les objets que les développeurs souhaitent manipuler plutôt que sur
+ la logique requise pour les manipuler.
+
+ La première étape de la OOP consiste à collecter tous les objets qu'un programmeur souhaite manipuler 
+ et à identifier comment ils sont liés les uns aux autres - un exercice souvent appelé modélisation de données .
+
+ Une fois qu'un objet est connu, il est étiqueté avec une classe d'objets qui définit le type de données
+ qu'il contient et toutes les séquences logiques qui peuvent le manipuler. Chaque séquence logique distincte
+ est appelée méthode.
+'''
+
+#CLASS
+'''Une classe est comme un constructeur d'objet, ou un "plan" pour créer des objets.
+
+ Un plan pour définir la manière dont quelque chose doit être défini. Il ne contient en fait aucune donnée. 
+ Les classes définissent des fonctions appelées méthodes , qui identifient les comportements et les actions
+ qu'un objet créé à partir de la classe peut effectuer avec ses données.
+
+'''
