4f03d7bbb974dc273fc51246cf91711e3f8f84f7
sauve <ngampiosauvet@gmail.com>
2021-02-02 19:51:09 +0300

    new

---
+++ PYTHON/fonctions.py
@@ -0,0 +1,18 @@
+def maxlist(l:list):
+    max = l[0]
+    for i in l:
+        if i > max:
+            max = i
+    print(f'le plus grand nombre = {max}')
+
+liste = [87, 23, 34, 44,234]
+maxlist(liste)
+
+def minlist(l:list):
+    min = l[0]
+    for i in l:
+        if i < min:
+            min = i
+    return f'le plus petit nombre = {min}'
+
+print(minlist(liste))
--- PYTHON/types_variables.py
+++ PYTHON/types_variables.py
@@ -20,7 +20,7 @@ mot = 'sauvet007'
  e.g: x += 1 ou x = x + 1 
 
  Les nombres, les chaînes de caractères,  et les tuple sont des types immuables
- 
+
  Leurs contenus ne peuvent pas être modifié après sa création; ils peuvent donc être
  utilisé comme clé de dictionnaire ou comme élément d'un autre ensemble.
 """
@@ -91,3 +91,11 @@ print(s)
 ss = {34, True, True, 3.5, "34"}
 print(ss)
 print(len(ss))
+
+# module 
+'''On appele la fonction minlist() et maxlist() depuis le module test 
+'''
+from fonctions import maxlist
+
+maliste = [45, 654, 87, 542, 0]
+maxlist(maliste)
