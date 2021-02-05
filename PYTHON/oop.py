c7f725bdb248ce00952619abaf90348302a87ca1
sauve <ngampiosauvet@gmail.com>
2021-02-05 13:43:00 +0300

    oop

--- PYTHON/oop.py
+++ PYTHON/oop.py
@@ -15,10 +15,41 @@
 '''
 
 #CLASS
-'''Une classe est comme un constructeur d'objet, ou un "plan" pour créer des objets.
+'''Une classe est un plan ou un prototype défini par l'utilisateur à partir duquel des objets sont créés
+ Une classe est comme un constructeur d'objet, ou un "plan" pour créer des objets.
 
  Un plan pour définir la manière dont quelque chose doit être défini. Il ne contient en fait aucune donnée. 
  Les classes définissent des fonctions appelées méthodes , qui identifient les comportements et les actions
  qu'un objet créé à partir de la classe peut effectuer avec ses données.
 
+ ATTRIBUTS:
+   Les attributs sont les variables appartenant à une classe
+ ATTRIBUTS D'INSTANCE :
+   La valeur d'un attribut d'instance est spécifique à une instance particulière de la classe.
+   La fonction __init __ () pour attribuer des valeurs aux propriétés de l'objet ou à d'autres opérations nécessaires
+   lors de la création de l'objet
+   Les propriétés que tous les objets doivent avoir sont définies dans la méthode appelée .__init__()
+ ATTRIBUTS DE CLASS :
+    les attributs de classe sont des attributs qui ont la même valeur pour toutes les instances de classe.
+    Les attributs de classe sont définis directement sous la première ligne du nom de classe(en dehors de .__init__() ) 
+    en attribuant une valeur à un nom de variable 
+  Les attributs sont toujours publics et sont accessibles à l'aide de l'opérateur point (.). Exemple: Myclass.Myattribute
+  
+ METHODES:
+  Les méthodes sont des fonctions définies dans une classe.
+
+ La création d'une nouvelle classe crée un nouveau type d'objet, permettant de créer de nouvelles instances de ce type
+'''
+
+#INSTANCE
+'''Une instance est un objet qui est construit à partir d'une classe et contient des données réelles
+
+ une classe est comme un formulaire ou un questionnaire. Une instance est comme un formulaire rempli
+ d'informations.
+
+  Chaque instance de classe peut avoir des attributs qui lui sont attachés pour conserver son état.
+  Les instances de classe peuvent également avoir des méthodes (définies par leur classe) pour modifier leur état.
+
+  Lorsqu'une nouvelle instance de classe est créée, l'instance est automatiquement passée au selfparamètre 
+  dans .__init__()   afin que de nouveaux attributs puissent être définis sur l'objet.
 '''
